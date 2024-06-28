5c089ab4567b7a2c1789976e8b8e8cf813ee0440
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2024-06-27 23:40:45 -0300

    add cors config

--- src/main/java/com/example/MeliUrlShorter/config/RedisConfiguration.java
+++ src/main/java/com/example/MeliUrlShorter/config/RedisConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.MeliUrlShorter.dbConfig;
+package com.example.MeliUrlShorter.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
