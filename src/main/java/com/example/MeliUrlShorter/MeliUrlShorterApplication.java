1791b3e68b3ba43d364c0cfcc64a3864aa0bcc48
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2024-06-27 23:30:44 -0300

    enable all cors

--- src/main/java/com/example/MeliUrlShorter/MeliUrlShorterApplication.java
+++ src/main/java/com/example/MeliUrlShorter/MeliUrlShorterApplication.java
@@ -3,9 +3,11 @@ package com.example.MeliUrlShorter;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableAdminServer
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class MeliUrlShorterApplication {
 
 	public static void main(String[] args) {
