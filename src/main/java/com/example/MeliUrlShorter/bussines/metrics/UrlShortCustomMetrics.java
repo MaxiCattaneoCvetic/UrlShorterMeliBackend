89e5dc8d45df04f5a97b30547d8dc08ad7e2c86d
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2024-07-02 23:33:12 -0300

    first app version

--- src/main/java/com/example/MeliUrlShorter/bussines/metrics/UrlShortCustomMetrics.java
+++ src/main/java/com/example/MeliUrlShorter/bussines/metrics/UrlShortCustomMetrics.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 
 @Component
-public class UrlShortCustomMetrics extends AppenderBase<ILoggingEvent> {
+public class UrlShortCustomMetrics {
     /*
             Descripcion de la clase -> Configuramos las metricas para medir con actuator.
             MasterRegistry nos permite registrar las metricas custom y el post lo que hace es postearlas en nuestra interfaz.
@@ -25,8 +25,6 @@ public class UrlShortCustomMetrics extends AppenderBase<ILoggingEvent> {
     //Aca añadimos las variables para contar las request y las respuestas
     private Counter postAndPutRequestCounter;
     private Counter getRequestCounter;
-    private Counter saveCounterDatabase;
-    private Counter getCounterDatabase;
     private Counter logCacheCounter;
 
     @Autowired
@@ -44,14 +42,6 @@ public class UrlShortCustomMetrics extends AppenderBase<ILoggingEvent> {
                 .description("Total de peticiones de obtención (GET) realizadas")
                 .register(meterRegistry);
 
-        saveCounterDatabase = Counter.builder("custom.crud.save.count")
-                .description("Cantidad de operaciones de persistencia en la base de datos")
-                .register(meterRegistry);
-
-        getCounterDatabase = Counter.builder("custom.crud.find.count")
-                .description("Cantidad de operaciones de búsqueda en la base de datos")
-                .register(meterRegistry);
-
         logCacheCounter = Counter.builder("cache.log.entries.found")
                 .description("Cantidad de veces que se encontraron entradas en el cache")
                 .register(meterRegistry);
@@ -67,26 +57,6 @@ public class UrlShortCustomMetrics extends AppenderBase<ILoggingEvent> {
         getRequestCounter.increment();
     }
 
-    public void incrementCrudSaveCount() {
-        saveCounterDatabase.increment();
-    }
-
-    public void incrementCrudFindCount() {
-        getCounterDatabase.increment();
-    }
 
 
-    // Tengo que implementar logica para contar las entradas en el cache y medirlas para saber cuantas veces utiliza el cache dividido a la cantidad de peticiones
-
-
-    @Override
-    protected void append(ILoggingEvent eventObject) {
-//        String message = eventObject.getFormattedMessage();
-//        logger.debug("Log received: {}", message);
-//
-//        if (message.contains("Cache entry for key") || message.contains("found in cache(s)")) {
-//            logCacheCounter.increment();
-//            logger.debug("Cache log entry found");
-//        }
-    }
 }
